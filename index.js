74ff51dac672a481f26c53dd4fce5911b6d13825
levchenko-yarya <levchenkoyarya@gmail.com>
2021-08-06 12:21:46 +0300

    view messages

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ const io = new Server(server)
 io.on('connection', (socket) => {
     console.log('a user connected')
     socket.on('chat message', (msg)=> {
+
+        io.emit('chat message', msg)
+
         console.log('message: ' + msg)
     })
 })
